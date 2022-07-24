80a62a5b25dd73f4dd7d51caccde1744fdf59ea9
olegator <o.shashko@icloud.com>
2022-07-24 21:41:33 +0300

    + errors for mid,left and right derivative in file derivative.py

--- derivative.py
+++ derivative.py
@@ -79,4 +79,35 @@ for i in range(1, m):
         a.itemset(i, i + 1, h)
 p = (y[i + 1] - 2 * y[i] + y[i - 1] for i in range(1, 99))
 
+
+def approximation_error(points_count: int, left_border: int, right_border: int) -> tuple:
+    step = abs(left_border - right_border) / points_count
+    x_values = [left_border + step * i for i in range(0, points_count)]
+    origin_y = [origin_dy(x_value) for x_value in x_values]
+    approx_y = [L_n(x_value, points_count) for x_value in x_values]
+    errors_mid = [abs(approx_y[i][0] - origin_y[i]) for i in range(0, points_count)]
+    errors_left = [abs(approx_y[i][1] - origin_y[i]) for i in range(0, points_count)]
+    errors_right = [abs(approx_y[i][2] - origin_y[i]) for i in range(0, points_count)]
+    max_mid_error = max(errors_mid)
+    max_left_error = max(errors_left)
+    max_right_error = max(errors_right)
+    print(points_count)
+    return max_mid_error, max_left_error, max_right_error
+
+
+points = [i for i in range(1, n)]
+errors_mid = [approximation_error(i, -3, 3)[0] for i in range(1, n)]
+errors_left = [approximation_error(i, -3, 3)[1] for i in range(1, n)]
+errors_right = [approximation_error(i, -3, 3)[2] for i in range(1, n)]
+
+plt.plot(points, errors_mid, label='mid error')
+plt.plot(points, errors_right, label='left error')
+plt.plot(points, errors_left, label='right error')
+
+plt.xlabel('x')
+plt.ylabel('f(x')
+plt.legend()
+
+plt.show()
+print("Processed!")
 # div = max(L_n(x_i[i])) - f(x_i[i] for i in range(0, m))
