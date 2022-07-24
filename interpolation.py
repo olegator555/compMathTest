32bf5e3b2b7555801d42ae77804ced01a9996329
olegator <o.shashko@icloud.com>
2022-07-24 21:15:17 +0300

    + error in file test4.py

--- interpolation.py
+++ interpolation.py
@@ -1,21 +1,23 @@
-import os
-
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import math
 
 
+# initial function data
+
 def f(x):
     return np.exp(-pow(x, 2))
 
 
-number_of_points = 100
+number_of_points = 5  # initial nodes number
 a = -3
 b = 3
 
 x_grid_values = [a + (b - a) / number_of_points * i for i in range(0, number_of_points + 1)]
 
+
+# spline function
 
 def spline_coeff(x: list) -> list:
     if len(x) == 0:
@@ -50,10 +52,10 @@ def spline_coeff(x: list) -> list:
     return l
 
 
-# print(spline_coeff(x_grid_values))
+l = spline_coeff(x_grid_values)
+
 
 def spline(x_value: float, origin_x: list):
-    l = spline_coeff(origin_x)
     step = origin_x[1] - origin_x[0]
     ans = -1
     if a <= x_value <= b:
@@ -64,10 +66,45 @@ def spline(x_value: float, origin_x: list):
                 continue
             break
 
-    print(ans)
+    # print(ans)
     return l[0][ans] + l[1][ans] * (x_value - origin_x[ans]) + l[2][ans] * ((x_value - origin_x[ans]) ** 2) + l[3][
         ans] * ((x_value - origin_x[ans]) ** 3)
 
+
+# Lagrange polynomial
+
+def l_i(x, n, j):
+    p1 = 1
+    p2 = 1
+    for i in range(0, n + 1):
+        if i != j:
+            p1 *= x - x_grid_values[i]
+            p2 *= x_grid_values[j] - x_grid_values[i]
+    return p1 / p2
+
+
+def L_n(x, n):
+    s = 0
+    for i in range(0, n + 1):
+        s += f(x_grid_values[i]) * l_i(x, n, i)
+    return s
+
+
+# interpolation eror
+
+def x_values(point_num: int) -> list:
+    return [a + (b - a) / point_num * i for i in range(0, point_num + 1)]
+
+
+def max_approximation_error(number_of_points: int, a: float, b: float) -> float:  # create single error function
+    points = [a + (b - a) / number_of_points * i for i in range(0, number_of_points + 1)]
+    origin_y = [f(i) for i in points]
+    l_y = L_n(origin_y, number_of_points)
+    errors = [abs(points[i] - origin_y[i]) for i in range(0, number_of_points)]
+    return max(errors)
+
+
+# building plots
 
 points_count = 200
 x_ax = [a + (b - a) / points_count * i for i in range(0, points_count)]
@@ -75,30 +112,13 @@ f_initial = [f(x_element) for x_element in x_ax]
 spline_values = []
 for i in range(0, points_count):
     spline_values.append(spline(x_ax[i], x_grid_values))
+lagrange_interpolation = [L_n(i, number_of_points) for i in x_ax]
 
 plt.plot(x_ax, f_initial, label='initil f(x)')
 plt.plot(x_ax, spline_values, label='spline')
+plt.plot(x_ax, lagrange_interpolation, label='Lagrange interp.')
+
 plt.xlabel('x')
 plt.ylabel('f(x')
 plt.legend()
 plt.show()
-
-
-def approximation_error(points_count: int, left_border: int, right_border: int) -> float:
-    x_ax = [a + (b - a) / points_count * i for i in range(0, points_count)]
-    f_initial = [f(x_element) for x_element in x_ax]
-    spline_values = []
-    for i in range(0, points_count):
-        spline_values.append(spline(x_ax[i], x_grid_values))
-    errors = [abs(spline_values[i] - f_initial[i]) for i in range(0, points_count)]
-    return max(errors)
-
-
-points = [i for i in range(1, points_count)]
-errors = [approximation_error(points, -3, 3) for points in points]
-plt.plot(points, errors, label='error')
-plt.xlabel('x')
-plt.ylabel('f(x')
-plt.legend()
-plt.show()
-print("Processed")
