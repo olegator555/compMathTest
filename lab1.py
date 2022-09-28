b6ec6be11566c1db601eec30ab9ca4493b626f0a
Олейник Владислав <null>
2022-09-28 20:14:59 +0300

    + lab1.py fixed x_grid_ch

--- lab1.py
+++ lab1.py
@@ -10,12 +10,22 @@ def f(x):
     return 1 / (1 + 25 * x ** 2)
 
 
-number_of_points = 6  # initial nodes number
-points_count = 60
+def x_grid_ch(n):
+    ch = [-1]
+    for i in range(1, n):
+        ch.append(np.cos((2 * i - 1) / (2 * (n - 1)) * np.pi))
+    ch.append(1)
+    ch.sort()
+    return ch
+
+
+number_of_points = 200  # initial nodes number
+points_count = number_of_points * 10
 a = -1
 b = 1
 
 x_grid_values = [a + (b - a) / number_of_points * i for i in range(0, number_of_points + 1)]
+x_grid_ch_values = x_grid_ch(number_of_points)
 
 
 def l_i(x, n, j):
@@ -23,19 +33,19 @@ def l_i(x, n, j):
     p2 = 1
     for i in range(0, n + 1):
         if i != j:
-            p1 *= x - x_grid_values[i]
-            p2 *= x_grid_values[j] - x_grid_values[i]
+            p1 *= x - x_grid_ch_values[i]
+            p2 *= x_grid_ch_values[j] - x_grid_ch_values[i]
     return p1 / p2
 
 
 def L_n(x, n):
     s = 0
     for i in range(0, n + 1):
-        s += f(x_grid_values[i]) * l_i(x, n, i)
+        s += f(x_grid_ch_values[i]) * l_i(x, n, i)
     return s
 
 
-x_ax = [a + (b - a) / points_count * i for i in range(0, points_count)]
+x_ax = [a + (b - a) / points_count * i for i in range(0, points_count + 1)]
 f_initial = [f(x_element) for x_element in x_ax]
 
 lagrange_interpolation = [L_n(i, number_of_points) for i in x_ax]
