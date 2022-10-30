7ee22ccf5cf529c32e2913e97d4f086ee3d81940
Шашко Олег Владимирович <null>
2022-10-30 20:10:22 +0300

    + lab2.py (1,2)

--- lab2.py
+++ lab2.py
@@ -34,4 +34,25 @@ plt.legend()
 plt.show()
 
 
+def composite_simpson(a: float, b: float, number_of_points: int, f):
+    h = (b - a) / number_of_points
+    k1 = 0
+    k2 = 0
+    for i in range(1, number_of_points, 2):
+        k1 += f(a + i * h)
+        k2 += f(a + (i + 1) * h)
+    return h / 3 * (f(a) + 4 * k1 + 2 * k2)
+
+
+n_values = np.arange(3, 9999, 10)
+origin_integral_value = (9*pow(np.pi, 2)-4)/27
+simpson_error_values = []
+for i in n_values:
+    simpson_error_values.append(abs(composite_simpson(0, np.pi, i, g2) - origin_integral_value))
+
+plt.plot(np.log(n_values), np.log(simpson_error_values), label='Approximation Error Simpson')
+plt.xlabel('x')
+plt.ylabel('f(x')
+plt.legend()
+plt.show()
 
