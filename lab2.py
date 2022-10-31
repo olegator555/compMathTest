8a022995e649710400dbff7f912042ce988affc0
Шашко Олег Владимирович <null>
2022-10-31 23:50:13 +0300

    + lab2 changed

--- lab2.py
+++ lab2.py
@@ -1,12 +1,9 @@
 import matplotlib
-import numpy
-import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
+import sympy
+from sympy import *
 from sympy.solvers import solve
-from sympy import Symbol
-from decimal import Decimal
-import scipy.integrate as integrate
 
 
 def g1(x):
@@ -30,15 +27,13 @@ approximation_errors = []
 for value in g1_der_values:
     approximation_errors.append(abs(value - origin_g1_der))
 
-
 # matplotlib.use('TkAgg')
-# plt.loglog(h_values, approximation_errors, label='Approximation Error')
-# plt.xlabel('x')
-# plt.ylabel('f(x')
-# plt.legend()
+plt.loglog(h_values, approximation_errors, label='Approximation Error')
+plt.xlabel('x')
+plt.ylabel('f(x')
+plt.legend()
 
-
-# plt.show()
+plt.show()
 
 
 def composite_simpson(a: float, b: float, number_of_points: int, f):
@@ -51,20 +46,21 @@ def composite_simpson(a: float, b: float, number_of_points: int, f):
     return h / 3 * (f(a) + 4 * k1 + 2 * k2)
 
 
-n_values = np.arange(3, 9999, 10)
-origin_integral_value = (9 * pow(np.pi, 2) - 4) / 27
-simpson_error_values = []
-for i in n_values:
-    simpson_error_values.append(abs(composite_simpson(0, np.pi, i, g2) - origin_integral_value))
-
-
+# n_values = np.arange(3, 9999, 10)
+# origin_integral_value = (9 * pow(np.pi, 2) - 4) / 27
+# simpson_error_values = []
+# for i in n_values:
+#     simpson_error_values.append(abs(composite_simpson(0, np.pi, i, g2) - origin_integral_value))
+#
+#
 # plt.loglog(n_values, simpson_error_values, label='Approximation Error Simpson')
 # plt.xlabel('x')
 # plt.ylabel('f(x')
 # plt.legend()
 
 
-# plt.show()
+plt.show()
+
 
 def gauss_func(x):
     return (63 * x ** 5 - 70 * x ** 3 + 15 * x) / 8
@@ -73,16 +69,32 @@ def gauss_func(x):
 def mul_func(x, x_values):
     for i in range(0, 5):
         mul = 1
-        for j in range(0,5):
+        for j in range(0, 5):
             if i != j:
-                mul *= (x-x_values[j])/(x_values[i]-x_values[j])
+                mul *= (x - x_values[j]) / (x_values[i] - x_values[j])
 
 
-def gauss_quad(x):
-    x_arg = Symbol('x')
-    x_values = solve('(63 * x ** 5 - 70 * x ** 3 + 15 * x) / 8', x_arg)
+def sympy_function(func: sympy.Expr, var_dict: dict) -> sympy.Expr:
+    new_dict = {key: sympy.Symbol(str(val)) for key, val in var_dict.items()}
+    result = func.subs(new_dict)
+    return result
+
+
+def gauss_quad():
     c = []
+    x = [0, np.sqrt(5 - 2 * np.sqrt((10 / 7))) / (-3), np.sqrt(5 - 2 * np.sqrt((10 / 7))) / 3,
+         np.sqrt(5 + 2 * np.sqrt((10 / 7))) / (-3),
+         np.sqrt(5 + 2 * np.sqrt((10 / 7))) / 3]
+    for i in range(0, 5):
+        mul = ""
+        x_arg, xj, xi = sympy.symbols("x xj xi")
+        for j in range(0, 5):
+            func = sympy.Expr
+            if i != j:
+                mul += "(x-(%f))/(%f-(%f))" % (x[j], x[i], x[j])
+        x_arg = Symbol('x')
+        ans = sympy.solve(mul, x_arg)
+        print(ans)
 
-
-
-print(gauss_quad(2))
+print(gauss_quad())
+print("done!")
