e245ee83584aa00676575b98e2eac138e78d592e
Шашко Олег Владимирович <null>
2022-11-16 15:24:52 +0300

    + lab3 lu decomposition

---
+++ lab3.py
@@ -0,0 +1,21 @@
+import numpy
+import numpy as np
+
+
+def lu(a, permute=False):
+    n = len(a)
+    u = np.zeros((n, n))
+    l = np.zeros((n, n))
+    p = np.ones((n,n))
+    m = np.zeros((n, n))
+    for i in range(0, n):
+        m.itemset((i, i), 1)
+    for j in range(0, n):
+        for i in range(j + 1, n):
+            m.itemset((i, j), -a[j, i] / a[j, j])
+    u = np.matmul(a, m)
+    return u
+
+
+a = np.array([[1, 1, 0, -3], [2, 1, -1, 1], [3, -1, -1, 2], [-1, 2, 3, -1]])
+print(lu(a))
