033e37e424c8e93db44ffaaac36c3b74fd25b1b0
Шашко Олег Владимирович <null>
2022-11-20 22:56:15 +0300

    + lab3 lu decomposition

--- lab3.py
+++ lab3.py
@@ -1,20 +1,112 @@
+import numpy
 import numpy as np
 
 
-def lu(a, permute=False):
-    n = len(a)
-    l = np.zeros((n, n))
-    p = np.ones((n, n))
-    u = a
-    for j in range(0, n):
-        m_i = np.zeros((n, n))
-        for i in range(0, n):
-            m_i.itemset((i,i),1)
-        for i in range(j + 1, n):
-            m_i.itemset((i, j), -a[j, i] / a[j, j])
-        u = np.matmul(u, m_i)
-    return u
+def max_value_and_index(a):
+    max = 0
+    max_ind = 0
+    for i in range(0, len(a)):
+        if a[i] > max:
+            max = a[i]
+            max_ind = i
+    return max_ind
 
 
-a = np.array([[1, 1, 0, -3], [2, 1, -1, 1], [3, -1, -1, 2], [-1, 2, 3, -1]])
-print(lu(a))
+def lu(a, permute):
+    # m_n = []
+    if not permute:
+        n = len(a)
+        u = a
+        # u = np.zeros((n, n))
+        l = np.identity(n)
+        # p = np.ones((n,n))
+        # m = np.zeros((n, n))
+        for j in range(0, n):
+            m_j = np.identity(n)
+            for i in range(j + 1, n):
+                m_j.itemset((i, j), -a[j, i] / a[j, j])
+                l.itemset((i, j), a[j, i] / a[j, j])  # plus, and these expressions are for the l matrix
+            # m_n.append(m_i)
+            u = np.matmul(m_j, u)
+        # u = np.matmul(np.linalg.inv(l), a)
+        # u = a
+        # for i in range(len(m_n))
+        # u = np.matmul(m_n[i], u)
+        return [l, u]
+    else:
+        n = len(a)
+        u = a
+        # u = np.zeros((n, n))
+        l = np.identity(n)
+        p = np.identity(n)
+        # m = np.zeros((n, n))
+        for j in range(0, n):
+            m_j = np.identity(n)
+            for i in range(j + 1, n):
+                if abs(a[j, j] - a[j - 1, j]) > 10e-10:
+                    m_j.itemset((i, j), -a[j, i] / a[j, j])
+                    l.itemset((i, j), a[j, i] / a[j, j])  # plus, and these expressions are for the l matrix
+                else:
+                    print('check')
+                    max_ind = max_value_and_index(a[i][i:])
+                    print(f"max_ind = ", max_ind)
+                    tmp2 = a[i, j]
+                    a[i, j] = a[max_ind, j]
+
+                    a[max_ind, j] = tmp2
+
+                    tmp2 = p[i, j]
+                    p[i, j] = p[max_ind, j]
+                    p[max_ind, j] = tmp2
+                    m_j.itemset((i, j), -a[j, i] / a[j, j])
+                    l.itemset((i, j), a[j, i] / a[j, j])
+            # m_n.append(m_i)
+            u = np.matmul(m_j, u)
+        # u = np.matmul(np.linalg.inv(l), a)
+        # u = a
+        # for i in range(len(m_n))
+        # u = np.matmul(m_n[i], u)
+        return [l, u, p]
+
+
+# a = np.array([[1, 1, 0, -3], [2, 1, -1, 1], [3, -1, -1, 2], [-1, 2, 3, -1]])
+a1 = np.array([[1, 2, 0], [1, 2, 1], [0, 1, 2]])
+# l = lu(a, False)[0]
+# u = lu(a, False)[1]
+tmp = lu(a1, True)
+l1 = tmp[0]
+u1 = tmp[1]
+
+# print(l)
+# print(u)
+# print(a)
+# print(np.matmul(l,u))
+
+print(l1)
+print(u1)
+print(tmp[2])
+print(np.matmul(tmp[2], a1))
+print(np.matmul(l1, u1))
+
+
+def solve(l, u, vec):
+    n = len(l)
+    b = np.matmul(l, vec)
+    y = []
+    x = []
+    for k in range(0, n):
+        sum = 0
+        for i in range(0, k - 1):
+            sum += l[k, i] * y[i]
+        y.append(b[k] - sum)
+    for k in range(0, n):
+        sum = 0
+        for i in range(k + 1, n):
+            sum += u[k, i] * x[i]
+        x.append(y[k] / u[k, k] - sum / u[k, k])
+    return x
+
+
+vector = [-16, 12, 39]
+x = solve(l1, u1, vector)
+print(x)
