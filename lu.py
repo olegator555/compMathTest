4b67704e6e72174741242a7ae58a24b008e36abd
Шашко Олег Владимирович <null>
2022-12-09 22:25:37 +0300

    added tsk 6_9

--- lu.py
+++ lu.py
@@ -1,33 +1,35 @@
 import numpy as np
+import scipy as scipy
 
 
 def lu_decomposition(a):
     n = len(a)
     l = np.zeros([n, n], float)
     u = np.zeros([n, n], float)
-    u[1, 1] = a[1, 1]
+    u[1, 1] = a[1][1]
     for i in range(0, n):
         l[i, i] = 1
     for j in range(2, n):
-        u[1, j] = a[1, j]
-        l[j, 1] = a[j, 1] / u[1, 1]
+        u[1, j] = a[1][j]
+        l[j, 1] = a[j][1] / u[1, 1]
     for i in range(2, n):
         sum = 0
         for k in range(1, i - 1):
             sum += l[i, k] * u[k, i]
-        u[i, i] = a[i, i] - sum
+        u[i, i] = a[i][i] - sum
         for j in range(i + 1, n):
             sum = 0
             for k in range(1, i - 1):
                 sum += l[i, k] * u[k, j]
-            u[i, j] = a[i, j] - sum
+            u[i, j] = a[i][j] - sum
             sum = 0
             for k in range(1, i - 1):
                 sum += l[j, k] * u[k, j]
-            l[i, j] = 1 / u[i, i] * (a[j, i] - sum)
+            l[i, j] = 1 / u[i, i] * (a[j][i] - sum)
 
     print("l = ", l)
     print("u = ", u)
+    return l, u
 
 
 def lu(a):
@@ -48,9 +50,27 @@ def lu(a):
 
     print(l)
     print(u)
+    return l, u
 
 
-a = np.array([[7, 3, -11], [-6, 7, 10], [-11, 2, -2]], float)
-vector = np.array([3, 2, 1])
-lu(a)
-lu_decomposition(a)
+def lu1(a):
+    u = np.zeros((len(a), len(a)))
+    l = np.zeros((len(a), len(a)))
+    m = np.zeros((len(a), len(a)))
+    for i in range(0, len(a)):
+        m.itemset(i, i, 1)
+    for i in range(0, len(a)):
+        for j in range(0, len(a)):
+            if abs(a[j][i]) > 10e-10 and i > j:
+                m.itemset((i, j), -(a[i][j] / a[j][i]))
+
+    u = np.multiply(a, m)
+    # l = np.linalg.inv(m)
+    return u
+
+
+a = np.array([[1, 1, 0, 3], [2, 1, -1, 1], [3, -1, -1, 2], [-1, 2, 3, -1]])
+wrong = np.array(lu1(a))
+print("lu processed")
+correct = np.array(scipy.linalg.lu(np.array(a), permute_l=False))
+print("done")
